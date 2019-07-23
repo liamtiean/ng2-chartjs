5301dfe909aa8b12903e05eb227e6419e4fb786d
Tien Tran <office05@rmsnext.com>
2019-07-23 16:05:51 +0700

    update height for maintainAspectRatio option

--- src/chart-js/chart-js.component.ts
+++ src/chart-js/chart-js.component.ts
@@ -4,23 +4,35 @@ import { ChartJSInterface } from '../chart-js-interfaces';
 
 @Component({
   selector: 'chartjs',
-  template: '<div><canvas></canvas></div>',
-  styles: [`
-    div {
-      display: inline-block;
-      width: 100%;
-    }
-  `]
+  template: '<div [ngStyle]="getStyle()"><canvas></canvas></div>',
+  styles: [
+    `
+      div {
+        display: inline-block;
+        width: 100%;
+      }
+    `
+  ]
 })
-
 export class ChartJsComponent implements AfterViewInit {
+  @Input() public height: number = null;
   @Input() public data: ChartJSInterface;
   private chart: Chart;
 
-  constructor(private el: ElementRef) {
-  }
+  constructor(private el: ElementRef) {}
 
   public ngAfterViewInit() {
-    this.chart = new Chart(this.el.nativeElement.querySelector('canvas').getContext('2d'), this.data);
+    this.chart = new Chart(
+      this.el.nativeElement.querySelector('canvas').getContext('2d'),
+      this.data
+    );
+  }
+  public getStyle() {
+    if (this.height === null) {
+      return {};
+    }
+    return {
+      height: `${this.height}px`
+    };
   }
 }
